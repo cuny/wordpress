ef5640aeab58ad7366a3050e181e1d2894336956
kubicue <3x2media@gmail.com>
2014-11-26 10:48:35 -0500

    Changes on README.md

--- wp-content/themes/cuny-5.5/template-blank.php
+++ wp-content/themes/cuny-5.5/template-blank.php
@@ -5,7 +5,7 @@ Template Name: Blank Page
 ?>
 
 <?php get_header(); ?>
-	<div id="wrap-content">
+	<div id="wrap-content" role="main">
 		<?php the_post(); the_content(); ?>
 	</div>
 <?php get_footer(); ?>
